d3e3f623a486a4230d6d5e92937b62ef11dd023b
FelixLemayBegin <FelixLemayBegin>
2024-12-06 14:34:36 -0500

    Remise Finale

--- assets/script.js
+++ assets/script.js
@@ -35,6 +35,9 @@ function disableCards() {
   secondCard.removeEventListener('click', flipCard);
 
   resetBoard();
+
+  // Vérifie si les cartes sont retourner
+  FiniOuNon();
 }
 
 function unflipCards() {
@@ -45,6 +48,9 @@ function unflipCards() {
     secondCard.classList.remove('flip');
 
     resetBoard();
+
+    // Vérifie si les cartes sont retourner
+    FiniOuNon();
   }, 1500);
 }
 
@@ -61,3 +67,85 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+/*Mon code*/
+/*===============================================================================================*/
+
+/*variables*/
+let debutButon = document.getElementById("debut-compteur");
+let temps = document.getElementById("temps");
+// Variables pour le cronomètre
+let intervalId = null;
+  //Variables pour la convertion du temps
+  let secondes = 0;
+  let minutes = 0;
+  let heures = 0;
+
+// Active le timer quand on appuie sur le bouton commencer
+debutButon.addEventListener("click", () =>
+{
+  if (intervalId) return;
+    {
+      // Ajoute +1 aux secondes a chaque secondes
+      intervalId = setInterval(() => {
+      secondes++;
+      let lettreEnNombre = parseInt(temps.value);
+      ConvertirTemps(secondes);
+    }, 1000);
+  }
+});
+
+// Cette fonction sert à vérifier si toutes les cartes sont retournées
+// Si toutes les cartes sont retournées, sa désactive le timer
+//J'ai mit cette fonction dans disablecard et unflipcard
+function FiniOuNon()
+{
+  if (Array.from(cards).every(cards => cards.classList.contains("flip")))
+  {
+    clearInterval(intervalId);
+  }
+}
+
+// Convertit le temps (secondes) en minutes quabd le timer atteind 60 secondes. Quand sa atteint 60 secondes, sa ajoute 1 minutes
+// Sa affiche aussi le temps dans la input du cronomètre
+function ConvertirTemps()
+{
+  if(secondes == 60)
+  {
+    secondes = 0;
+    minutes++;
+  }
+  temps.value = minutes + ":" + secondes;
+}
+
+
+// Pour le dialog
+//
+//
+//
+/*=========================================*/
+
+const dialog = document.getElementById("dialog");
+const fermerFenetre = document.getElementById("fermerFenetre");
+const fermerIndefiniment = document.getElementById("nePlusAfficherCetteFenetre");
+
+// Vérification du localStorage pour savoir si on doit afficher le dialog
+if(localStorage.getItem("nePlusAfficher") !== "true")
+{
+  // Affiche le dialogue si "nePlusAfficher" n'est pas défini ou n'est pas "true"
+  dialog.showModal();
+}
+
+
+// Fonction pour fermer la fenêtre temporairement
+function FermerFenetre()
+{
+    dialog.close();
+}
+
+// Fonction pour ne plus afficher la fenêtre définitivement
+function FermerIndefiniment()
+{
+  localStorage.setItem("nePlusAfficher", "true");  
+  FermerFenetre();
+}
