e2ac0fbc925809f81d31a4b28e5d02986763af31
FelixLemayBegin <FelixLemayBegin>
2025-05-20 17:29:20 -0400

    mettre index a ce qui fAUT

--- index.php
+++ index.php
@@ -5,10 +5,6 @@ require ('include/entete.inc');
 if (isset($_GET['message'])) {
     if ($_GET['message'] === 'ajoutE-ok') {
         echo '<p class="confirmation">Votre equipe a bien été créée !</p>';
-    } elseif ($_GET['message'] === 'ajoutR-ok') {
-        echo '<p class="confirmation">Votre résultat a bien été ajouté !</p>';
-    } elseif ($_GET['message'] === 'envoi-ok') {
-        echo '<p class="confirmation">Votre message a bien été envoyé. Merci !</p>';
     } elseif ($_GET['message'] === 'erreur') {
         echo '<p class="erreur">Une erreur est survenue. Veuillez réessayer plus tard.</p>';
     }
@@ -21,16 +17,18 @@ if ($stmt) {
     if ($stmt->rowCount() > 0) {
         while ($enreg = $stmt->fetch(PDO::FETCH_NUM)) {
             echo '<div class="item first">';
-            echo '  <div class="content">';
-            echo '    <div class="caption">';
-            echo '      <div class="middle">';
-            echo '        <i class="fa-solid fa-shield"></i>';
-            echo '      </div>';
-            echo '      <div class="title">' . htmlspecialchars($enreg[0]) . '</div>';
-            echo '      <div class="subtitle">' . nl2br(htmlspecialchars($enreg[1])) . '</div>';
-            echo '    </div>';
-            echo '  </div>';
-            echo '  <a href="details-equipe.php?id=' . $enreg[2] . '" class="boutton-style">Joueurs</a>';
+            echo '<a href="#">';
+            echo '<div class="content">';
+            echo '</div>';
+            echo '<div class="caption">';
+            echo '<div class="middle">';
+            echo '<i class="fa-solid fa-shield"></i>';
+            echo '</div>';
+            echo "<div class='title'>$enreg[0]</div>";
+            echo "<div class='subtitle'>$enreg[1]</div>";
+            echo '</div>';
+            echo " <a href='details-equipe.php?id=" . $enreg[2] . "' class='boutton-style'>Joueurs</a> ";
+            echo '</a>';
             echo '</div>';
         }
     } else {
